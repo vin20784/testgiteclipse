db08b7ae383f72f070e7aecf3e8cc05ee430908e
vin20784 <vin20784@gmail.com>
2019-10-13 23:49:32 +0530

    first git checkin thru eclipse

---
+++ src/vnzrestapi/testvnzrestapi.java
@@ -0,0 +1,11 @@
+package vnzrestapi;
+
+public class testvnzrestapi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Vihaan!");
+	}
+
+}
