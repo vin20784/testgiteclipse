eff7ef32b11ba59a9e807b2232d2e91aad65f384
vin20784 <vin20784@gmail.com>
2019-10-13 23:58:02 +0530

    another git test class to see uodate

---
+++ src/vnzrestapi/testgithelloworld.java
@@ -0,0 +1,11 @@
+package vnzrestapi;
+
+public class testgithelloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("what is GIT ?");
+	}
+
+}
