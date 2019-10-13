7edf7d7a0b8b86e93196ff6386a5dfc909f19ab8
vin20784 <vin20784@gmail.com>
2019-10-14 00:37:25 +0530

    someting

--- src/vnzrestapi/testgithelloworld.java
+++ src/vnzrestapi/testgithelloworld.java
@@ -5,7 +5,13 @@ public class testgithelloworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("what is GIT ?");
+		//System.out.println("what is GIT ?");
+		for (int i=0;i<20;i++)
+		{
+			System.out.println(i);
+			
+		}
+		}
 	}
 
-}
+
